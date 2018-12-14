3855f50a461326a702032bc9646290fc7d6a0ae4
Joey-mehne <joseph.mehne@stu.jefferson.kyschools.us>
2018-12-14 08:14:22 -0500

    overwatch

--- dmg_data.py
+++ dmg_data.py
@@ -1,5 +1,5 @@
 import csv
-from matplotlib import pylot as plt
+from matplotlib import pyplot as plt
 
 filename = 'Overwatch_edopic.csv'
 with open(filename) as f:
@@ -9,10 +9,17 @@ with open(filename) as f:
 
     dps_char = []
     for row in reader:
-        dps = int(row[1])
-        dps_char.append(dps)
+        try:
+            dps = int(row[1])
+            dps_char.append(dps)
+        except ValueError:
+            dps = 0
+            dps_char.append(dps)
 
     print(dps_char)
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
 
 fig = plt.figure(dpi=128, figsize=(10, 6))
 plt.plot(dps_char, c='red')
@@ -22,6 +29,3 @@ plt.xlabel('', fontsize=16)
 plt.ylabel("Damage", fontsize=16)
 plt.tick_params(axis='both', which='major', labelsize=16)
 plt.show()
-
-    for index, column_header in enumerate(header_row):
-        print(index, column_header)
