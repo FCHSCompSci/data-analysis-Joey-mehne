ce7c2fa952d6394c351e12cc27dd2fe87d9bbe1b
Joey-mehne <joseph.mehne@stu.jefferson.kyschools.us>
2018-12-14 07:52:28 -0500

    overwatch damage data

---
+++ dmg_data.py
@@ -0,0 +1,27 @@
+import csv
+from matplotlib import pylot as plt
+
+filename = 'Overwatch_edopic.csv'
+with open(filename) as f:
+    reader = csv.reader(f)
+    header_row = next(reader)
+    print(header_row)
+
+    dps_char = []
+    for row in reader:
+        dps = int(row[1])
+        dps_char.append(dps)
+
+    print(dps_char)
+
+fig = plt.figure(dpi=128, figsize=(10, 6))
+plt.plot(dps_char, c='red')
+
+plt.title("Overwatch Hero's Damage per Second", fontsize=24)
+plt.xlabel('', fontsize=16)
+plt.ylabel("Damage", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.show()
+
+    for index, column_header in enumerate(header_row):
+        print(index, column_header)
