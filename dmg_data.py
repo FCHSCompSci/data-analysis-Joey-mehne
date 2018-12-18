2a45282ef30cac9cedcbecf0225ad55cd8b46d18
Joey-mehne <joseph.mehne@stu.jefferson.kyschools.us>
2018-12-18 08:31:06 -0500

    overwatch damage data updated

--- dmg_data.py
+++ dmg_data.py
@@ -8,22 +8,22 @@ with open(filename) as f:
     print(header_row)
 
     dps_char = []
+    char_nme = []
     for row in reader:
         try:
+            nme = str(row[0])
+            char_nme.append(nme)
             dps = int(row[1])
             dps_char.append(dps)
         except ValueError:
+            nme = str(row[0])
+            char_nme.append(nme)
             dps = 0
             dps_char.append(dps)
 
     print(dps_char)
-
-    char_nme = []
-    for row in reader:
-        nme = row[0]
-        char_nme.append(nme)
-
     print(char_nme)
+
 
     for index, column_header in enumerate(header_row):
         print(index, column_header)
@@ -31,10 +31,9 @@ with open(filename) as f:
 fig = plt.figure(dpi=128, figsize=(10, 6))
 print(len(char_nme))
 print(len(dps_char))
-# plt.scatter(char_nme, dps_char, c='red')
-#
-# plt.title("Overwatch Hero's Damage per Second", fontsize=24)
-# plt.xlabel('Hero Name', fontsize=16)
-# plt.ylabel("Damage", fontsize=16)
-# plt.tick_params(axis='both', which='major', labelsize=16)
-# plt.show()
+plt.plot(dps_char, c='red')
+plt.title("Overwatch Hero's Damage per Second", fontsize=24)
+plt.xlabel('Hero Name', fontsize=16)
+plt.ylabel("Damage", fontsize=16)
+plt.tick_params(axis='both', which='major', labelsize=16)
+plt.show()
